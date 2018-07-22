c28d584e280a8c35c65592b96e6ea928857854fa
Maiko Naruse <maimai@Maiko-no-MacBook-Air.local>
2018-07-22 18:36:49 +0900

    メッセージ送信機能の非同期化

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,53 @@
+$(function(){
+
+  function appendMessage(message){
+    var img = message.image ? `<img src=${ message.image }>` : "";
+
+    var html = `<div class="message" data-message-id="${ message.id }">
+                <div class="message__name">
+                  ${ message.name }
+                </div>
+                <div class="message__date">
+                  ${ message.created_at }
+                </div>
+                <div class="message__content">
+                  <div class="upper_message">
+                  ${ message.body }
+                  </div>
+                  <div class="lower_message">
+                  ${ img }
+                  </div>
+                </div>
+              </div>`
+    return html;
+  }
+
+  function pageScrollDown(target){
+    target.animate({scrollTop: message_list[0].scrollHeight}, 'fast');
+  }
+
+  $('#new_message').submit(function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(message){
+      if (message.length !== 0){
+      appendMessage(message);
+      $('#new_message')[0].reset();
+      }
+      $('.form__button').prop('disabled', false);
+    })
+    .fail(function(){
+      alert('メッセージが送られていません。');
+      $('.form__button').prop('disabled', false);
+    })
+  });
+});
